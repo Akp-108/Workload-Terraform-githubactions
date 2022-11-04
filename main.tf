bc326c69d62e4da8fd7b3ed2d233747f9776b306
Akp-108 <akp1081@gmail.com>
2022-11-04 18:27:37 +0530

    change

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "newrelic" {
 }
 
 resource "newrelic_workload" "test_workload" {
-    name = "Test Workload1"
+    name = "Test Workload2"
     account_id = 3649334 
 
     entity_guids = ["MzY0OTMzNHxJTkZSQXxOQXw0MDk4NTc0ODc1NzI4NTM1Mjc"]
